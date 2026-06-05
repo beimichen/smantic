fc7b2cc359e62c2c2f4cda09ece1c0925d195deb
Bei Mi Chen <beimichen@gmail.com>
2026-06-05 23:52:08 +1000

    bump version to 0.1.1

--- smantic/__init__.py
+++ smantic/__init__.py
@@ -35,7 +35,7 @@ from .adapters import (
 from .chunker import Chunk, StructureAwareChunker
 from .ir import BBox, Document, Element, Page
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "chunk_document",
